99162c5b109cfac92ebcee0dfcd3b0e86768f885
Raúl Javierre <javierreraul@gmail.com>
2020-10-25 11:04:00 +0100

    Primeros ejemplos de testing con Mocha y NYC

---
+++ node/utils_example.js
@@ -0,0 +1,12 @@
+exports.sum = function (a, b) {
+    if(a==b) {
+        return b+b;
+    }
+    return a + b;
+}
+exports.substract = function (a, b) {
+    if (a == b) {
+        return 0;
+    }
+    return a - b;
+}
