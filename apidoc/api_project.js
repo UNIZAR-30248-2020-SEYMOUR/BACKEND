1cae3f0cdc632e2db2bca4c7fd4aae105bb8b021
Kifixo <danielhuici@hotmail.com>
2020-12-28 20:23:36 +0000

    Actualizar ApiDoc

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-12-25T21:28:43.849Z",
+    "time": "2020-12-28T20:23:17.255Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
